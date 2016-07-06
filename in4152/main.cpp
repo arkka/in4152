dd9bdb08a4c9f5a403bf1b155b05f33304ea0e7a
Arkka Dhiratara <arkka.d@gmail.com>
2016-07-07 01:09:25 +0200

    Initial Commit

---
+++ in4152/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  in4152
+//
+//  Created by Arkka Dhiratara on 7/7/16.
+//  Copyright © 2016 Dhiratara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
